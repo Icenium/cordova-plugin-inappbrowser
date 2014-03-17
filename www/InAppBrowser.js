f9909f84e795d422225b456bb3795e65e27408b1
Nadya Atanasova <nadya.it@gmail.com>
2014-03-17 15:08:05 +0200

    Make url to open absolute

--- www/InAppBrowser.js
+++ www/InAppBrowser.js
@@ -77,16 +77,21 @@ InAppBrowser.prototype = {
 };
 
 module.exports = function(strUrl, strWindowName, strWindowFeatures) {
-    var iab = new InAppBrowser();
-    var cb = function(eventname) {
-       iab._eventHandler(eventname);
-    };
-
     // Don't catch calls that write to existing frames (e.g. named iframes).
     if (window.frames && window.frames[strWindowName]) {
         var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
         return origOpenFunc.apply(window, arguments);
     }
+
+	//make the path absolute
+    var anchorEl = document.createElement('a');
+    anchorEl.href = strUrl;
+    strUrl = anchorEl.href;
+
+    var iab = new InAppBrowser();
+    var cb = function (eventname) {
+    iab._eventHandler(eventname);
+    };
 
     exec(cb, cb, "InAppBrowser", "open", [strUrl, strWindowName, strWindowFeatures]);
     return iab;
